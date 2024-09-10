e99be77ba6aea5894ff5559637b69d1569ed56a1
n4mlz <n4mlz.dev@gmail.com>
2024-09-10 20:42:26 +0900

    test

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,25 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -28,4 +47,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     f.read_to_string(&mut contents)?;
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
